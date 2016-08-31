b4c5f42c39d862165a26660f6fadcc3466818f7e
Tim Mac <timwlindsey@gmail.com>
2016-08-31 18:25:13 -0500

    base boilerplate. react/webpack/hotreload/eslint

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	devtool: 'eval',
+	//Have to add the dev server and hot reloading server
+	entry: [
+	  'webpack-dev-server/client?http://0.0.0.0:3000',
+	  'webpack/hot/only-dev-server',
+	  './app/index'
+	],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js',
+		publicPath: '/static/'
+	},
+	resolve: {
+    	extensions: ['', '.js', '.jsx']
+  	},
+	//Tell webpack to use hot reloader on the components
+	module: {
+		loaders: [{ 
+			test: /\.jsx?$/, 
+			loaders: ['react-hot', 'babel'], 
+			include: path.join(__dirname, 'app') 
+		}]
+	},
+	//Add the got module replacement plugin
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
+};
