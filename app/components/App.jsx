aaf75e4046d26b5a7ed426dd6ae8bf58f8934c4c
Tim Mac <timwlindsey@gmail.com>
2016-09-01 19:51:33 -0500

    alter component structure a bit

---
+++ app/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Todos from './Todos';
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+	      Hello from App!
+	      <Todos/>
+      </div>
+    );
+  }
+});
